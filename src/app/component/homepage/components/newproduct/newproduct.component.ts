3ed168472cf766c003eedcab524deebfed4caa24
SadeghBh <sogandbahrami7799@gmail.com>
2024-01-19 21:13:26 +0330

    product.

--- src/app/component/homepage/components/newproduct/newproduct.component.ts
+++ src/app/component/homepage/components/newproduct/newproduct.component.ts
@@ -9,8 +9,8 @@ import { DataserviceService } from 'src/app/services/data/data.service';
   styleUrls: ['./newproduct.component.css']
 })
 export class NewproductComponent {
-  constructor(private dataService: DataserviceService){ }
-  
+  constructor(private dataService: DataserviceService) { }
+
   // icons
   right = faAngleRight
   left = faAngleLeft
@@ -18,10 +18,69 @@ export class NewproductComponent {
 
   // carousel
   data: Product[] = this.dataService.product
-  
+  product: any = []
+  number: number = 0
+  slide!: Product[]
+  zIndex = 'z-1'
 
-  category(){
+  category(array1: any, array2: Product[], number: number) {
+    var i = 0
+    var num = array2.length
+    var floor = Math.floor(num / number)
 
+    var view: any = []
+    array2.forEach((value) => {
+      view.push(value)
+    }
+    )
+
+    while (i < floor) {
+      var x = view.splice(0, number)
+      array1.push(x)
+      i++
+    }
+    if (view.length != 0) {
+      array1.push(view)
+    }
+    this.slide = array1[this.number]
+  }
+
+  ngOnInit() {
+    if (window.innerWidth > 991.5) {
+      this.category(this.product, this.data, 6)
+    } else if (window.innerWidth > 767) {
+      this.category(this.product, this.data, 4)
+    } else {
+      this.category(this.product, this.data, 3)
+    }
+  }
+
+  next() {
+    this.number++
+    if (this.number >= this.product.length) {
+      this.number = 0
+    }
+    // animation
+    this.zIndex = 'z-3'
+
+    setTimeout(() => {
+      this.zIndex = 'z-1'
+    }, 150)
+    this.slide = this.product[this.number]
+  }
+
+  prev() {
+    this.number--
+    if (this.number <= -1) {
+      this.number = this.product.length - 1
+    }
+    // animation
+    this.zIndex = 'z-3'
+
+    setTimeout(() => {
+      this.zIndex = 'z-1'
+    }, 150)
+    this.slide = this.product[this.number]
   }
 
 }
